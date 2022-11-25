363ee115350fd06cda78f1279aacef2d2de6c32f
MaiaraGRocha <maiaragrocha@hotmail.com>
2022-11-25 18:33:53 -0300

    repetições JS

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,17 @@
+
+// Estrutura de repetição com teste lógico no início:
+
+var c = 1
+    while(c <= 6){
+        console.log(`Passo ${c}`)
+        c++
+    }
+
+//Mesma coisa, so que um copiando e colando (mais trabalho) e outro usando LAÇÕS DE REPETIÇÃO
+
+     /*console.log(`Passo ${c}`)
+     console.log(`Passo ${c}`)
+     console.log(`Passo ${c}`)
+     console.log(`Passo ${c}`)
+     console.log(`Passo ${c}`)
+     console.log(`Passo ${c}`)*/ 
---
+++ exercicios/aula14/ambiente03.js
@@ -0,0 +1,17 @@
+/*
+var c  = 1
+while ( c <= 5){
+    console.log(c)      // estrutura 'while'
+    c++
+}*/
+
+/*for (var c  = 1; ( c <= 6); c++ ){
+    console.log(c)                      // estrutura 'for'
+}*/
+
+
+console.log ('Vai começar...')
+for(var c = 1; c<=4; c++){
+    console.log(c)
+}
+console.log('FIM!')

e0cb4a95a1945ec6e09d45b7e4703d8dd6cdcb88
MaiaraGRocha <maiaragrocha@hotmail.com>
2022-12-01 10:18:11 -0300

    desafio

---
+++ exercicios/aula14ex/desafio contar/contar.js
@@ -0,0 +1,7 @@
+function contar(){
+    let inic = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res =  document.getElementById('res')
+    
+}

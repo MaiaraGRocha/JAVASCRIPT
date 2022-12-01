c4dccbe8c5f93bb64d66bbb3ffe5d0cb0237daeb
MaiaraGRocha <maiaragrocha@hotmail.com>
2022-12-01 11:54:00 -0300

    desafio contagem pronto

--- exercicios/aula14ex/desafio contar/contar.js
+++ exercicios/aula14ex/desafio contar/contar.js
@@ -4,4 +4,33 @@ function contar(){
     let passo = document.getElementById('txtp')
     let res =  document.getElementById('res')
     
+    // para testar o JS e se estiver faltando algum dado, aparece o 'alert'
+    if(inic.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] falta dados...')
+    }else{
+        res.innerHTML = 'Contando:'
+        
+        //convertendo a caixa de texto em numero:
+        let i = Number(inic.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(i < f){
+            for( let c = i; c <= f; c += p ){
+                res.innerHTML += ` ${c} \u{1F449} `
+            }
+            res.innerHTML+= `\u{1F3C1}`
+        }else{
+                for ( let c = i; c >= f; c -= p){
+                    res.innerHTML+= `${c} \u{1F449}`
+                }
+                res.innerHTML+= `\u{1F3C1}`
+            }
+        
+        
+    }
+
+
+
+        
+    
 }

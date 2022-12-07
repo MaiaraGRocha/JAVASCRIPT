92f3413479c6c6e77846a1dd753afd4eb2b9225a
MaiaraGRocha <maiaragrocha@hotmail.com>
2022-12-07 08:30:33 -0300

    desafio 03

---
+++ exercicios/desafio03/script.js
@@ -0,0 +1,19 @@
+    
+let num = document.getElementById('numb')
+let val = document.getElementById('value')
+let res = document.getElementById('resul')
+let valores = [] // esse vetor é p analizar os dados
+    
+function adicionar(){  
+  
+
+    if(num.value.length == 0 || num.value.length >100){
+       window.alert('[ERRO] Digite um número válido')
+    }else{
+       
+    }
+
+    
+
+
+}
